5ffb183c02884a660aa96f1f70c15f9512cc9840
ciro <ciro@ciro-K45VM>
2019-10-28 18:56:31 -0300

    Refatorando para utilizar Spring Data JPA e camada de serviço

--- food-api/src/main/java/com/oric/food/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/oric/food/api/controller/CozinhaController.java
@@ -1,6 +1,7 @@
 package com.oric.food.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,11 +9,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oric.food.domain.model.Cozinha;
 import com.oric.food.domain.repository.CozinhaRepository;
+import com.oric.food.domain.service.CadastroCozinhaService;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -21,25 +26,36 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
+	private CadastroCozinhaService cadastroCozinha;
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
 		
-		return cozinhaRepository.todas();
+		return cozinhaRepository.findAll();
 	}
 	
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
 		
-		Cozinha cozinha = cozinhaRepository.porId(id);
+		Optional<Cozinha> cozinha = cozinhaRepository.findById(id);
 		
-		if( cozinha != null) {
+		if( cozinha.isPresent()) {
 			
-			return ResponseEntity.ok(cozinha);
+			return ResponseEntity.ok(cozinha.get());
 		}
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		
+		return cadastroCozinha.salvar(cozinha);
+	}
+	
+	
 	
 
 }
