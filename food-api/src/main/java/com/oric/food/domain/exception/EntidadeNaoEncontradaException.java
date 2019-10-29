801f13427d73f89d73a7a12fc678103e33e0a401
ciro <ciro@ciro-K45VM>
2019-10-29 12:58:41 -0300

    Criando exceção para a entidade

---
+++ food-api/src/main/java/com/oric/food/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package com.oric.food.domain.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntidadeNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+
+}
