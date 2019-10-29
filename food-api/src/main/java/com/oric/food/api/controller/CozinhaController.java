020c320eabdb93c5f5d88f1e9fa1d6546471d6a4
ciro <ciro@ciro-K45VM>
2019-10-28 23:49:12 -0300

    Implementação método atualizar

--- food-api/src/main/java/com/oric/food/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/oric/food/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.oric.food.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,6 +28,7 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
+	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -53,6 +56,23 @@ public class CozinhaController {
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		
 		return cadastroCozinha.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+		
+		
+		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+		
+		if(cozinhaAtual.isPresent()) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(),"id");
+			
+			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
+			return ResponseEntity.ok(cozinhaSalva);
+			
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	
--- food-api/src/main/java/com/oric/food/domain/model/Cozinha.java
+++ food-api/src/main/java/com/oric/food/domain/model/Cozinha.java
@@ -7,8 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -24,7 +22,7 @@ public class Cozinha {
 	private Long id;
 	
 	//@JsonIgnore
-	@JsonProperty(value = "titulo")
+	//@JsonProperty(value = "titulo")
 	@Column(nullable = false)
 	private String nome;
 	
