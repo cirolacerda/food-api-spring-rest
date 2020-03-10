65ebbe7fe475e231ae074caeba24c0d467ce325a
ciro <ciro@ciro-K45VM>
2020-03-09 21:40:21 -0300

    Implementando o controller de estado

---
+++ food-api/src/main/java/com/oric/food/api/controller/EstadoController.java
@@ -0,0 +1,22 @@
+package com.oric.food.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oric.food.domain.repository.EstadoRepository;
+import com.oric.food.domain.service.CadastroEstadoService;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
+	
+	
+
+}
