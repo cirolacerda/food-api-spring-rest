4ae73d40ac98c93fd1872a7db4f747caa5253fba
ciro <ciro@ciro-K45VM>
2020-03-13 23:07:39 -0300

    Estendendo o JpaRepository e customizando o repositório base

--- food-api/src/main/java/com/oric/food/domain/repository/RestauranteRepository.java
+++ food-api/src/main/java/com/oric/food/domain/repository/RestauranteRepository.java
@@ -1,13 +1,12 @@
 package com.oric.food.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.oric.food.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 	
 	
 
