737af4dcddab8025a9e0b843058ec6db78751987
ciro <ciro@ciro-K45VM>
2020-03-15 10:55:14 -0300

    Criando entidade produto

---
+++ food-api/src/main/java/com/oric/food/domain/model/Produto.java
@@ -0,0 +1,46 @@
+package com.oric.food.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table
+@Entity
+public class Produto {
+	
+	@EqualsAndHashCode.Include
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurante_id", nullable = false)
+	private Restaurante restaurante;
+	
+	
+
+}
