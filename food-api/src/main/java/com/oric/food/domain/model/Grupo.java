dfb8b4965fc848d0ff76a59942acef4ce6272b80
ciro <ciro@ciro-K45VM>
2020-03-15 17:35:29 -0300

    Implementando Entidade Grupo

---
+++ food-api/src/main/java/com/oric/food/domain/model/Grupo.java
@@ -0,0 +1,35 @@
+package com.oric.food.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name ="grupo_id"), inverseJoinColumns = @JoinColumn(referencedColumnName = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
+
+}
