bbb34b9c63f3fb4924be2bb26a29f5ac68846ffe
ciro <ciro@ciro-K45VM>
2020-03-15 17:13:19 -0300

    Mapeando entidade Produto

--- food-api/src/main/java/com/oric/food/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/oric/food/api/controller/RestauranteController.java
@@ -56,7 +56,7 @@ public class RestauranteController {
 		
 		if(restauranteAtual.isPresent()) {
 			
-			BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco", "dataCadastro");
+			BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 			
 			Restaurante restauranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());
 			
