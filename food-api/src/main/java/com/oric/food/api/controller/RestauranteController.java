b41416f86e18a972e9445f45db12027a442d8cc7
ciro <ciro@ciro-K45VM>
2020-03-29 21:40:39 -0300

    Refatorando para usar exceção Negocio Exception

--- food-api/src/main/java/com/oric/food/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/oric/food/api/controller/RestauranteController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oric.food.domain.exception.EntidadeNaoEncontradaException;
+import com.oric.food.domain.exception.NegocioException;
 import com.oric.food.domain.model.Restaurante;
 import com.oric.food.domain.repository.RestauranteRepository;
 import com.oric.food.domain.service.CadastroRestauranteService;
@@ -18,40 +23,57 @@ import com.oric.food.domain.service.CadastroRestauranteService;
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
-	
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
-	
+
 	@GetMapping
-	public List<Restaurante> listar(){
-		
+	public List<Restaurante> listar() {
+
 		return restauranteRepository.findAll();
 	}
-	
-	@GetMapping("/{restauranteId}")
-	public Restaurante buscar( @PathVariable("restauranteId") Long id ) {
-		
-		return cadastroRestaurante.buscarOuFalhar(id);
-		
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			return cadastroRestaurante.salvar(restaurante);
+
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
+
 	}
-	
-	
+
+	@GetMapping("/{restauranteId}")
+	public Restaurante buscar(@PathVariable("restauranteId") Long id) {
+
+		return cadastroRestaurante.buscarOuFalhar(id);
+
+	}
+
 	@PutMapping("/{restauranteId}")
-	public Restaurante atualizar( @PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
-	
+	public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+		BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro",
+				"produtos");
 		
-					
-			BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-			
-			
-			
+		try {
+	
 			return cadastroRestaurante.salvar(restauranteAtual);
 		
+		} catch (EntidadeNaoEncontradaException e) {
+			
+			throw new NegocioException(e.getMessage());
+		}
 		
+		
+
 	}
 
 }
