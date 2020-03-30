654b175afbd304e1f9b04006e89f9610837ee383
ciro <ciro@ciro-K45VM>
2020-03-30 10:49:49 -0300

    Trantando de forma global exception entidade em uso

--- food-api/src/main/java/com/oric/food/api/exceptionhandler/ApiExceptionHandler.java
+++ food-api/src/main/java/com/oric/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.oric.food.domain.exception.EntidadeEmUsoException;
 import com.oric.food.domain.exception.EntidadeNaoEncontradaException;
 import com.oric.food.domain.exception.NegocioException;
 
@@ -43,6 +44,17 @@ public class ApiExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException (EntidadeEmUsoException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT)
+				.body(problema);
+		
 	}
 
 }
