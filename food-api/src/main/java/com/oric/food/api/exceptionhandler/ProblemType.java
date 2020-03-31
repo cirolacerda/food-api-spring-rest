a7ffd655be6e48fffb85ebd015ac4189c8acf332
ciro <ciro@ciro-K45VM>
2020-03-31 11:13:37 -0300

    Padronizando o formato de problemas no corpo de respostas com a RFC 7807

---
+++ food-api/src/main/java/com/oric/food/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,20 @@
+package com.oric.food.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+	
+	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada" );
+	
+	private String title;
+	private String uri;
+	
+	ProblemType(String path, String title) {
+		this.uri = "localhost:8080" + path;
+		this.title = title;
+	}
+	
+	
+
+}
