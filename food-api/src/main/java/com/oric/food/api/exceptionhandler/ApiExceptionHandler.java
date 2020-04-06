6c1f47cf288c38155278ef49c044db3844f20908
ciro <ciro@ciro-K45VM>
2020-04-06 19:49:22 -0300

    Customizando mensagens de validação globais em Resource Bundle

--- food-api/src/main/java/com/oric/food/api/exceptionhandler/ApiExceptionHandler.java
+++ food-api/src/main/java/com/oric/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +31,9 @@ import com.oric.food.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL
 	= "Ocorreu um erro interno inesperado no sistema. Tente novamente e se "
@@ -259,16 +265,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 		        HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-			ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+
+		    ProblemType problemType = ProblemType.DADOS_INVALIDOS;
 		    String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 		    
 		    BindingResult bindingResult = ex.getBindingResult();
 		    
 		    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-		    		.map(fieldError -> Problem.Field.builder()
+		    		.map(fieldError -> {
+		    			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+		    			
+		    			return Problem.Field.builder()
 		    				.name(fieldError.getField())
-		    				.userMessage(fieldError.getDefaultMessage())
-		    				.build())
+		    				.userMessage(message)
+		    				.build();
+		    		})
 		    		.collect(Collectors.toList());
 		    
 		    Problem problem = createProblemBuilder(status, problemType, detail)
