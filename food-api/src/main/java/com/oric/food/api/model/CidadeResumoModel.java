f67e717910096554c2c395e1ff95c30c4e235c13
ciro <ciro@ciro-K45VM>
2020-04-14 23:06:17 -0300

    Adicionando endereço no modelo da representação do restaurante

---
+++ food-api/src/main/java/com/oric/food/api/model/CidadeResumoModel.java
@@ -0,0 +1,16 @@
+package com.oric.food.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private String estado;
+
+}
--- food-api/src/main/java/com/oric/food/core/modelmapper/ModelMapperConfig.java
+++ food-api/src/main/java/com/oric/food/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.oric.food.api.model.RestauranteModel;
+import com.oric.food.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
 		
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+				
+				modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+                    .addMapping(restSrc -> restSrc.getEndereco().getCidade().getEstado().getNome(), 
+                	(restDest, val) -> restDest.getEndereco().getCidade().setEstado((String) val));
+		
+		return modelMapper;
 	}
 }
