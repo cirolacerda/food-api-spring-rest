dc77649111e758ace94b42528c567bcd34315222
ciro <ciro@ciro-K45VM>
2020-04-17 10:21:56 -0300

    Implementando os endpoints de produtos

---
+++ food-api/src/main/java/com/oric/food/domain/exception/ProdutoNaoEncontradoException.java
@@ -0,0 +1,22 @@
+package com.oric.food.domain.exception;
+
+public class ProdutoNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProdutoNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	
+	}
+	
+	public ProdutoNaoEncontradoException(Long restauranteId, Long produtoId) {
+		
+		this(String.format("Não existe um cadastro de produto com código %d para o restaurante com código %d",produtoId, restauranteId));
+		
+	}
+		
+
+}
---
+++ food-api/src/main/java/com/oric/food/domain/repository/ProdutoRepository.java
@@ -0,0 +1,24 @@
+package com.oric.food.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.oric.food.domain.model.Produto;
+import com.oric.food.domain.model.Restaurante;
+
+@Repository
+public interface ProdutoRepository extends CustomJpaRepository<Produto, Long> {
+	
+	@Query("from Produto where restaurante.id = :restaurante and id = :produto")
+    Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
+            @Param("produto") Long produtoId);
+    
+    List<Produto> findByRestaurante(Restaurante restaurante);
+    
+    
+
+}
