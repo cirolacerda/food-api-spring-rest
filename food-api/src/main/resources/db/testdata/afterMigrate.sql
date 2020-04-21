b055ea05e341d6af4f73a6595ddb4757d726dd22
ciro <ciro@ciro-K45VM>
2020-04-21 10:33:31 -0300

    Ajustando massa de dados usuário responsavel

--- food-api/src/main/resources/db/testdata/afterMigrate.sql
+++ food-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -81,4 +81,12 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (3, 'José Souza', 'jose.aux@food.com', '123', utc_timestamp),
 (4, 'Sebastião Martins', 'sebastiao.cad@food.com', '123', utc_timestamp); 
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);       
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+
+delete from restaurante_usuario_responsavel;
+
+insert into usuario (id, nome, email, senha, data_cadastro) values
+(5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp);
+
+insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5);
+
