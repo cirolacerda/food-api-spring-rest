73653cafd11b9e76fad179629733ccafea5ccad5
ciro <ciro@ciro-K45VM>
2020-04-23 23:18:13 -0300

    Otimizando a query de pedidos e retornando model resumido na listagem

--- food-api/src/main/java/com/oric/food/api/controller/PedidoController.java
+++ food-api/src/main/java/com/oric/food/api/controller/PedidoController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oric.food.api.assembler.PedidoModelAssembler;
+import com.oric.food.api.assembler.PedidoResumoModelAssembler;
 import com.oric.food.api.model.PedidoModel;
+import com.oric.food.api.model.PedidoResumoModel;
 import com.oric.food.domain.model.Pedido;
 import com.oric.food.domain.repository.PedidoRepository;
 import com.oric.food.domain.service.CadastroPedidoService;
@@ -27,11 +29,14 @@ public class PedidoController {
 	@Autowired
 	private PedidoModelAssembler pedidoModelAssembler;
 	
+	@Autowired
+	private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+	
 	@GetMapping
-	public List<PedidoModel> listar(){
+	public List<PedidoResumoModel> listar(){
 		List<Pedido> todosPedidos = pedidoRepository.findAll();
 		
-		return pedidoModelAssembler.toCollectionModel(todosPedidos);
+		return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
 	}
 	
 	@GetMapping("/{pedidoId}")
--- food-api/src/main/java/com/oric/food/domain/repository/PedidoRepository.java
+++ food-api/src/main/java/com/oric/food/domain/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package com.oric.food.domain.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oric.food.domain.model.Pedido;
 
 @Repository
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long> {
-
+	
+	@Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
+	List<Pedido> findAll(); 
 }
