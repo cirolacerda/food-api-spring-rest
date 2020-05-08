7dcce11d6bad38d26352bcf09a5c7a824c99b34b
ciro <ciro@ciro-K45VM>
2020-05-08 12:49:28 -0300

    Implementando endpoint de transição de status de pedidos

---
+++ food-api/src/main/java/com/oric/food/domain/service/FluxoPedidoService.java
@@ -0,0 +1,32 @@
+package com.oric.food.domain.service;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oric.food.domain.exception.NegocioException;
+import com.oric.food.domain.model.Pedido;
+import com.oric.food.domain.model.StatusPedido;
+
+@Service
+public class FluxoPedidoService {
+
+	@Autowired
+	private CadastroPedidoService emissaoPedido;
+	
+	@Transactional
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+					pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO.getDescricao()));
+		}
+		
+		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+		
+	}
+}
